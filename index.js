62187d980b11ed5b7c5b6ef66f1b750f34eea463
phoebe velma awuor <awuorphoebi@gmail.com>
2024-11-28 16:00:36 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = [ "Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+cats.push('Ralph');
+console.log (cats);
+}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+    console.log (cats);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+    console.log (cats);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+    console.log (cats);
+}
+function appendCat(name) {
+    return [...cats, name];
+}
+function prependCat(name) {
+    return [name, ...cats];
+}
+  
+  
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+  
+  
+function removeFirstCat() {
+    return cats.slice(1);
+}
